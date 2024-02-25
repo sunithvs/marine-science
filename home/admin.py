0ff1e66f86e6b7270bd038e4334c52e831339a86
sunithvs <sunithvs2002@gmail.com>
2024-02-25 07:01:10 +0530

    feat:  add theme to abstract

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Notice, Gallery, Seminar
+from .models import Notice, Gallery, Seminar, Journals, Publication, Facility
 
 admin.site.register(Notice)
 admin.site.register(Gallery)
 admin.site.register(Seminar)
+
+admin.site.register(Journals)
+admin.site.register(Publication)
+admin.site.register(Facility)
