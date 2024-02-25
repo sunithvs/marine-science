24b92e118194070889920bdcadd3592bf5567b24
sunithvs <sunithvs2002@gmail.com>
2024-02-25 07:31:00 +0530

    feat:  add theme to abstract

--- maricon/models.py
+++ maricon/models.py
@@ -33,13 +33,13 @@ THEMES = [" Oceans and Climate change",
           'Societal Applications of Marine-Geosciences',
           'Polar Sciences']
 
-def send_email(message, receiver, otp=None):
+
+def send_email(message, receiver, subject="Maricon Registration/Login verification"):
     port = 587  # For starttls
     smtp_server = "smtp.gmail.com"
     sender_email = EMAIL_HOST_USER
     receiver_email = receiver
     password = EMAIL_HOST_PASSWORD
-    subject = "Maricon Registration/Login verification"
     text = message
     message = f'Subject: {subject}\n\n{text}'
 
@@ -47,6 +47,8 @@ def send_email(message, receiver, otp=None):
     with smtplib.SMTP(smtp_server, port) as server:
         server.ehlo()  # Can be omitted
         server.starttls(context=context)
+        # utf-8 encoding is used to support special characters
+        message = message.encode('utf-8')
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
@@ -196,3 +198,11 @@ class PaperAbstract(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(
+            self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        super().save(force_insert, force_update, using, update_fields)
+        msg = (f"New Abstract {self.title}, Submitted  by {self.user.full_name} ({self.user.email})"
+               f" on the theme {self.theme} use the link to download the file https://marine.cusat.ac.in{self.file.url}")
+        threading.Thread(target=send_email, args=(msg, EMAIL_HOST_USER, "New abstract submission")).start()
