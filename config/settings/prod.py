8337e5caf6697030366c8e0dc31ff4950c7bdeeb
sunithvs <sunithvs2002@gmail.com>
2024-02-28 12:33:01 +0530

    fix: changed nginx.conf

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,7 +6,7 @@ Staging Settings for Django Project
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool('DEBUG', default=False)
 
 # Allowed hosts
 ALLOWED_HOSTS = [
